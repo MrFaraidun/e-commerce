ded753fad6dd103967f32e378359e76d583ec035
Faraidun <faraidunbahaden32@gmail.com>
2026-06-05 18:23:58 +0300

    config: prepare project for free deployment with env parameters

--- frontend/src/api/axiosConfig.js
+++ frontend/src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: `http://${window.location.hostname}:8080/backend/api`,
+  baseURL: import.meta.env.VITE_API_BASE_URL || `http://${window.location.hostname}:8080/backend/api`,
   headers: {
     "Content-Type": "application/json",
   },
